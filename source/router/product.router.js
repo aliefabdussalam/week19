37d4b32a2d6a58dc115475e37949e9326b905017
aliefabdussalam <aliefabdussalam766@gmail.com>
2021-09-20 16:53:41 +0700

    init for week20

--- source/router/product.router.js
+++ source/router/product.router.js
@@ -11,7 +11,7 @@ router
   .get('/product-all', getAllData)
   .post('/product/transaksi/', midauth, transaction)
   .get('/product/transaksi', getdataTransaction)
-  .get('/product', midauth, getlist)
+  .get('/product', getlist)
   .get('/product/:id', midauth, getdetail)
   .post('/product', midauth, authorization.isAdmin, upload, insert)
   .put('/product/:id', midauth, authorization.isAdmin, upload, update)
